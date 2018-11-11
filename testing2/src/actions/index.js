31567c75a524a6136937f2829f9d90ea7b387ef2
Katarzyna Bernad <katarzyna.bernad@netguru.co>
2018-11-11 18:59:06 +0100

    routes + auth

--- testing2/src/actions/index.js
+++ testing2/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { SAVE_COMMENT, FETCH_COMMENTS } from 'actions/types';
+import { SAVE_COMMENT, FETCH_COMMENTS, CHANGE_AUTH } from 'actions/types';
 
 export function saveComment(comment) {
   return { type: SAVE_COMMENT, payload: comment }
@@ -9,4 +9,8 @@ export function saveComment(comment) {
 export function fetchComments() {
   const response = axios.get("http://jsonplaceholder.typicode.com/comments")
   return { type: FETCH_COMMENTS, payload: response }
+}
+
+export function changeAuth(isLoggedIn) {
+  return { type: CHANGE_AUTH, payload: isLoggedIn }
 }
